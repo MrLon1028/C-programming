fc2197454b28543db526077affaa79dfc568e21e
Londemonai <londemonaicoc1@gmail.com>
2026-09-15 21:51:35 +0800

    Add if else to project

--- Cprogramming_1.c
+++ Cprogramming_1.c
@@ -8,7 +8,15 @@ int main() {
 
     for (int i = 0; i < ArrayL; i++) {
         printf("%d\n", myNumbers[i]);
-    }    
+    }
+    if (myNumbers[0] == 10) {
+        printf("The first element is 10\n");
+    } else {
+        printf("The first element is not 10\n");
+    }
+
     printf("Hello World\n");
+
+    
     return 0;
 }
