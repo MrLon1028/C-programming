75a3d1e651fc0e03c2d212324dcfa8276633de48
Londemonai <londemonaicoc1@gmail.com>
2026-09-15 21:38:06 +0800

    Add Array to project

--- Cprogramming_1.c
+++ Cprogramming_1.c
@@ -4,7 +4,11 @@ int main() {
 
     int myNumbers[] = {10, 25, 50, 75, 100, 125};
     printf("%zu\n", sizeof(myNumbers)); // 24 bytes
-    printf("%zu\n", sizeof(myNumbers) / sizeof(myNumbers[0])); // 6 elements
+    int ArrayL = sizeof(myNumbers) / sizeof(myNumbers[0]); // 6 elements
+
+    for (int i = 0; i < ArrayL; i++) {
+        printf("%d\n", myNumbers[i]);
+    }    
     printf("Hello World\n");
     return 0;
 }
